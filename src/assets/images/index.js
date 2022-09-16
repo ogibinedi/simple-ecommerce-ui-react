62d68ebb7c05e516be50c44adfe958b059394bbb
Ogi Bin Edi <ogibinedi@gmail.com>
2022-09-16 23:52:01 +0700

    remove image raw

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,18 +1,18 @@
-import Slide1 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342764/samples/ecommerce/slide3_mzacbr.jpg';
-import Slide2 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342759/samples/ecommerce/slide1_kqvyqs.jpg';
-import Slide3 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342757/samples/ecommerce/slide2_pvn0zx.jpg';
-import Product1 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342766/samples/ecommerce/product2_ze2lpz.png';
-import Product2 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342765/samples/ecommerce/product3_pi40lj.png';
-import Product3 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342761/samples/ecommerce/product8_uwlfyl.png';
-import Product4 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342760/samples/ecommerce/product6_vlwsa9.png';
-import Product5 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342757/samples/ecommerce/product5_lmdwp8.png';
-import Product6 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342752/samples/ecommerce/product1_ofzxzn.png';
-import Product8 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342748/samples/ecommerce/product4_fpukvf.png';
-import Product9 from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342757/samples/ecommerce/product9_eag3aj.png';
-import Payment from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342748/samples/ecommerce/payment_i8vidl.png';
-import Brand from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342748/samples/ecommerce/brand2_hm8zsm.png';
-import BgRegister from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342751/samples/ecommerce/bg_register_wue0ov.png';
-import BgLogin from 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342747/samples/ecommerce/bg_login_j1k50q.png';
+const Slide1 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342764/samples/ecommerce/slide3_mzacbr.jpg';
+const Slide2 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342759/samples/ecommerce/slide1_kqvyqs.jpg';
+const Slide3 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342757/samples/ecommerce/slide2_pvn0zx.jpg';
+const Product1 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342766/samples/ecommerce/product2_ze2lpz.png';
+const Product2 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342765/samples/ecommerce/product3_pi40lj.png';
+const Product3 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342761/samples/ecommerce/product8_uwlfyl.png';
+const Product4 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342760/samples/ecommerce/product6_vlwsa9.png';
+const Product5 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342757/samples/ecommerce/product5_lmdwp8.png';
+const Product6 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342752/samples/ecommerce/product1_ofzxzn.png';
+const Product8 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342748/samples/ecommerce/product4_fpukvf.png';
+const Product9 = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342757/samples/ecommerce/product9_eag3aj.png';
+const Payment = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342748/samples/ecommerce/payment_i8vidl.png';
+const Brand = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342748/samples/ecommerce/brand2_hm8zsm.png';
+const BgRegister = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342751/samples/ecommerce/bg_register_wue0ov.png';
+const BgLogin = 'https://res.cloudinary.com/cloudimageobe/image/upload/v1663342747/samples/ecommerce/bg_login_j1k50q.png';
 
 export { 
     Slide1, 
