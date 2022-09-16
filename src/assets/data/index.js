c481ee296681277061cfa67cb5def3f214fb4bd5
Ogi Bin Edi <ogibinedi@gmail.com>
2022-09-16 22:28:16 +0700

    upload assets file

---
+++ src/assets/data/index.js
@@ -0,0 +1,90 @@
+import {
+    Slide1,
+    Slide2,
+    Slide3, 
+    Product1, 
+    Product2, 
+    Product3, 
+    Product4, 
+    Product5, 
+    Product6,
+    Product8,
+    Product9
+} from '../images';
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: Slide1,
+        title: 'BIG SALE 50%',
+        desc: 'Macam macam Jaket Kulit Jangan Sampai Kehabisan Buruan Mampir Kesini Banyak Diskon Menunggu Anda!',
+        bg: 'f5fafd'
+    },
+    {
+        id: 2,
+        img: Slide2,
+        title: 'BIG SALE 50%',
+        desc: 'Berbagai Macam Digital Camera Jangan Sampai Kehabisan Buruan Mampir Kesini Banyak Diskon Menunggu Anda!',
+        bg: 'fcf1ed'
+    },
+    {
+        id: 3,
+        img: Slide3,
+        title: 'BIG SALE 50%',
+        desc: 'Berbagai Macam Sneaker Jangan Sampai Kehabisan Buruan Mampir Kesini Banyak Diskon Menunggu Anda!',
+        bg: 'f5fefc'
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb",
+        title: "SWEETER STYLE"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb",
+        title: "T-SHIRT STYLE"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/11929006/pexels-photo-11929006.jpeg?auto=compress&cs=tinysrgb",
+        title: "JEANS STYLE"
+    }
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: Product1
+    },
+    {
+        id: 2,
+        img: Product2
+    },
+    {
+        id: 3,
+        img: Product3
+    },
+    {
+        id: 4,
+        img: Product4
+    },
+    {
+        id: 5,
+        img: Product5
+    },
+    {
+        id: 6,
+        img: Product6
+    },
+    {
+        id: 7,
+        img: Product8
+    },
+    {
+        id: 8,
+        img: Product9
+    }
+];
---
+++ src/assets/images/index.js
@@ -0,0 +1,33 @@
+import Slide1 from './slide1.jpg';
+import Slide2 from './slide2.jpg';
+import Slide3 from './slide3.jpg';
+import Product1 from './product1.png';
+import Product2 from './product2.png';
+import Product3 from './product3.png';
+import Product4 from './product4.png';
+import Product5 from './product5.png';
+import Product6 from './product6.png';
+import Product8 from './product8.png';
+import Product9 from './product9.png';
+import Payment from './payment.png';
+import Brand from './brand2.png';
+import BgRegister from './bg_register.png';
+import BgLogin from './bg_login.png';
+
+export { 
+    Slide1, 
+    Slide2, 
+    Slide3, 
+    Product1, 
+    Product2, 
+    Product3, 
+    Product4, 
+    Product5, 
+    Product6,
+    Product8,
+    Product9,
+    Payment,
+    Brand,
+    BgLogin,
+    BgRegister
+ };
